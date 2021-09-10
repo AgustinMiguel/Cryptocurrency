93aa9d969bc286d7ae165a83ba13a7f904c3d5e1
Agustin Miguel <Agustin.Miguel@tsoftglobal.com>
2021-09-10 13:20:42 -0300

    bug fix

--- src/components/Coin.tsx
+++ src/components/Coin.tsx
@@ -3,21 +3,21 @@ import * as CurrencieService from "../services/CurrencieService";
 import { CryptoCurrency } from "../interfaces/InterfacesList";
 
 const Coin = ({ match }) => {
-  const [coin, setCoin] = useState<CryptoCurrency>([]);
+  const [coin, setCoin] = useState<CryptoCurrency>();
 
   useEffect(() => {
     CurrencieService.getCurrency(match.params.id).then(
       ({ data }: { data: CryptoCurrency }) => {
-        console.log(data);
         setCoin(data[0]);
+        console.log(coin);
       }
     );
   }, []);
 
   return (
     <React.Fragment>
-      <h1>{coin.name}</h1>
-      <p>Precio (USD:) {coin.price_usd}</p>
+      <h1>{coin?.name}</h1>
+      <p>Precio (USD:) {coin?.price_usd}</p>
     </React.Fragment>
   );
 };
